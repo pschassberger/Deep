d5e5a1c7c7b167fc3a27463cbfc3ebceb9527c50
pschassberger <pschassberger@gmail.com>
2020-07-01 14:59:18 -0600

    built NN class

--- NN-Minst/NN.py
+++ NN-Minst/NN.py
@@ -1,3 +1,12 @@
-# Writing a scratch NN to analyse the minst dataset
-# source: http://neuralnetworksanddeeplearning.com/chap1.html
+import numpy as np
+
+# creating a 2 layer NN
+# create NN class
+class NeuralNet:
+    def __init__(self, x, y):
+        self.input = x
+        self.weight1 = np.random.rand(self.input.shape[1], 4)
+        self.weight2 = np.random.rand(4, 1)
+        self.y = y
+        self.output = np.zeros(y.shape)
 
---
+++ NN-Minst/readme.txt
@@ -0,0 +1,7 @@
+
+
+This is a project to further learn about NN and Deep learning. 
+I will follow and implement a scratch NN algorithm to analyze the 
+Minst dataset. 
+source: https://towardsdatascience.com/how-to-build-your-own-neural-network-from-scratch-in-python-68998a08e4f6 
+
