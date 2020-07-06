d62177054b5da9517b28fb2f4b43c384edc30508
pschassberger <pschassberger@gmail.com>
2020-07-06 15:10:47 -0600

    added basic character with movement

--- NN/env.py
+++ NN/env.py
@@ -5,27 +5,49 @@
 
 import pygame
 
+
 pygame.init()
 
 #set up screen
 screen = pygame.display.set_mode([2000, 1500])
-
+# character
+x = 200
+y = 200
+width = 200
+height = 200
+velocity = 50
 # end with user prompt
 running = True
 while running:
-
+    # quit game condition
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     # Backround color
-    screen.fill((0, 0, 225))
+    screen.fill((0, 0, 0))
 
-    # character
-    pygame.draw.circle(screen, (225, 0, 0), (250, 250), 75)
+    # keys
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+        x -= velocity
 
-    # flip the display
-    pygame.display.flip()
+    if key[pygame.K_RIGHT]:
+        x += velocity
+
+    if key[pygame.K_UP]:
+        y -= velocity
+
+    if key[pygame.K_DOWN]:
+        y += velocity
+
+    # draw character
+    pygame.draw.rect(screen, (225, 0, 0), (x, y, width, height))
+    pygame.display.update()
+    # redraw old location
+    screen.fill((0,0,0))
+    # update display
+    
 
 # fin
 pygame.quit()
