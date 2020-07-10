fd38e46bfd634007fe118a29ce9f0b14765acef9
pschassberger <pschassberger@gmail.com>
2020-07-10 10:26:00 -0600

    cleaning

--- NN/agent.py
+++ NN/agent.py
@@ -1,10 +1,1 @@
 # create character class
-
-
-class Agent(object):
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.velovity = 25
